a71b259afb6d6da6fb6c147cb43cd5330f785fc2
dron2309 <andrei03jump@gmail.com>
2023-02-13 22:08:06 +0300

    practic

---
+++ Triangle/KlassTreug.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle
+{
+    internal class KlassTreug
+    {
+        static void Main(string[] args)
+        {
+            Treugolnick triangle = new Treugolnick();
+            Console.WriteLine("Введите сторону а1");
+            double a1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите сторону а2");
+            double a2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите сторону b1");
+            double b1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите сторону b2");
+            double b2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите сторону с1");
+            double c1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите сторону с2");
+            double c2 = double.Parse(Console.ReadLine());
+            triangle.TreugMet(a1, a2, b1, b2, c1, c2);
+            Console.WriteLine(triangle);
+        }
+    }
+}
---
+++ TriangleTests/TreugolnickTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Triangle;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle.Tests
+{
+    [TestClass()]
+    public class TreugolnickTests
+    {
+        [TestMethod()]
+        public void CheckingForNegativeNumbers()
+        {
+            Treugolnick triangle = new Treugolnick();
+            triangle.TreugMet(-10, -5, -50, -50, -1, -1);
+        }
+
+        public void CheckingForPositiveNumbers()
+        {
+            Treugolnick triangle = new Treugolnick();
+            triangle.TreugMet(10,5, 50,50, 1,1);
+        }
+        
+    }
+}
